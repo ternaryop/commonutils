52fdec0ba50a4a7ec48566cf50c558f17edf93e6
Davide Ficano <davide.ficano@gmail.com>
2015-04-15 16:26:46 +0200

    added the attribute style checkedColorFilter

--- app/src/main/java/com/ternaryop/widget/CheckableImageView.java
+++ app/src/main/java/com/ternaryop/widget/CheckableImageView.java
@@ -1,23 +1,39 @@
 package com.ternaryop.widget;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.util.AttributeSet;
 import android.widget.Checkable;
 import android.widget.ImageView;
 
 public class CheckableImageView extends ImageView implements Checkable {
+    public static final String PACKAGE_NAME = "http://schemas.android.com/apk/res-auto";
+    public static final int DEFAULT_CHECKED_COLOR_FILTER = 0x88000000;
     private boolean isChecked;
+    private int checkedColorFilter;
 
     public CheckableImageView(Context context) {
         super(context);
+        setup(null);
     }
 
     public CheckableImageView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        setup(attrs);
     }
 
     public CheckableImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setup(attrs);
+    }
+
+    private void setup(AttributeSet attrs) {
+        if (attrs == null) {
+            checkedColorFilter = DEFAULT_CHECKED_COLOR_FILTER;
+        } else {
+            checkedColorFilter = attrs.getAttributeIntValue(PACKAGE_NAME, "checkedColorFilter", DEFAULT_CHECKED_COLOR_FILTER);
+        }
     }
 
     @Override
@@ -28,7 +44,11 @@ public class CheckableImageView extends ImageView implements Checkable {
     @Override
     public void setChecked(boolean checked) {
         isChecked = checked;
-        setImageAlpha(isChecked ? 100 : 255);
+        if (checked) {
+            getDrawable().setColorFilter(checkedColorFilter, PorterDuff.Mode.SRC_ATOP);
+        } else {
+            getDrawable().setColorFilter(null);
+        }
     }
 
     @Override
