fa5e922f40f0763084dbd631bc7ec7ac8155d2c9
Davide Ficano <davide.ficano@gmail.com>
2017-11-01 07:34:34 +0100

    share content using URI and granting permission, this modification is necessary to work on API 24 or above

--- app/src/main/java/com/ternaryop/utils/intent/IntentUtils.java
+++ app/src/main/java/com/ternaryop/utils/intent/IntentUtils.java
@@ -1,6 +1,5 @@
 package com.ternaryop.utils.intent;
 
-import java.io.File;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -13,6 +12,7 @@ import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 
 import com.ternaryop.utils.dialog.app.AppPickerDialog;
 import com.ternaryop.utils.dialog.app.AppPickerUtils;
@@ -23,12 +23,12 @@ import com.ternaryop.utils.dialog.app.AppPickerUtils;
  * Helper class to create intents
  */
 public class IntentUtils {
-    public static Intent getAppIntentFromMimeType(PackageManager packageManager, String path, ComponentName componentName, String mimeType) {
+    public static Intent getAppIntentFromMimeType(Activity activity, Uri uri, ComponentName componentName, String mimeType) {
         if (componentName != null) {
-            Intent intent = createViewIntent(path, componentName, mimeType);
+            Intent intent = createViewIntent(activity, uri, componentName, mimeType);
 
             // check intent existence
-            List<ResolveInfo> list = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+            List<ResolveInfo> list = activity.getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
             if (list != null && list.size() > 0) {
                 return intent;
             }
@@ -36,23 +36,35 @@ public class IntentUtils {
         return null;
     }
 
-    public static Intent createViewIntent(String path, ComponentName componentName, String mimeType) {
+    public static Intent createViewIntent(Activity activity, Uri uriFile, ComponentName componentName, String mimeType) {
+//        return ShareCompat.IntentBuilder.from(activity)
+//                .setStream(uriFile) // uri from FileProvider
+//                .setType(mimeType)
+//                .getIntent()
+//                .setAction(Intent.ACTION_VIEW) //Change if needed
+//                .setDataAndType(uriFile, mimeType)
+//                .addFlags(Intent.FLAG_ACTIVITY_FORWARD_RESULT
+//                        | Intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP
+//                        | Intent.FLAG_RECEIVER_FOREGROUND
+//                        | Intent.FLAG_GRANT_READ_URI_PERMISSION);
         Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setDataAndType(Uri.fromFile(new File(path)), mimeType);
+        intent.setDataAndType(uriFile, mimeType);
         intent.setFlags(Intent.FLAG_ACTIVITY_FORWARD_RESULT
                 | Intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP
-                | Intent.FLAG_RECEIVER_FOREGROUND);
+                | Intent.FLAG_RECEIVER_FOREGROUND
+                | Intent.FLAG_GRANT_READ_URI_PERMISSION);
         intent.setComponent(componentName);
         return intent;
     }
 
-    public static void startViewActivity(final Activity activity,
-                                         final String path,
-                                         final String mimeType,
+    public static void startViewActivity(@NonNull final Activity activity,
+                                         @NonNull final Uri uri,
+                                         @NonNull final String mimeType,
                                          boolean useDefaultViewer) {
+        String path = uri.getPath();
         if (useDefaultViewer) {
             ComponentName componentName = AppPickerUtils.getDefaultViewerComponentName(activity, path, mimeType);
-            Intent intent = getAppIntentFromMimeType(activity.getPackageManager(), path, componentName, mimeType);
+            Intent intent = getAppIntentFromMimeType(activity, uri, componentName, mimeType);
 
             // use default saved viewer
             if (intent != null) {
@@ -67,7 +79,7 @@ public class IntentUtils {
             public void onClick(DialogInterface dialog, int which) {
                 ActivityInfo activityInfo = appPickerDialog.getSelectedApp().activityInfo;
                 ComponentName componentName1 = new ComponentName(activityInfo.applicationInfo.packageName, activityInfo.name);
-                activity.startActivity(createViewIntent(path, componentName1, mimeType));
+                activity.startActivity(createViewIntent(activity, uri, componentName1, mimeType));
             }
         });
         appPickerDialog.show();
