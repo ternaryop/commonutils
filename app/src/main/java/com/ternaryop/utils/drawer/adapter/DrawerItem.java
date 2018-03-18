b0017cc17c6941d9627f4be5dc170346e6041d5b
Davide Ficano <davide.ficano@gmail.com>
2018-03-18 10:35:32 +0100

    migrated from android.app.Fragment to android.support.v4.app.Fragment

--- app/src/main/java/com/ternaryop/utils/drawer/adapter/DrawerItem.java
+++ app/src/main/java/com/ternaryop/utils/drawer/adapter/DrawerItem.java
@@ -1,7 +1,7 @@
 package com.ternaryop.utils.drawer.adapter;
 
-import android.app.Fragment;
 import android.content.Context;
+import android.support.v4.app.Fragment;
 
 import com.ternaryop.utils.drawer.counter.CountRetriever;
 
