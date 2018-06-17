aa775c1c0317adaa3976a736396bb8d137641051
Davide Ficano <davide.ficano@gmail.com>
2018-06-17 10:42:47 +0200

    utility to handle dates in ISO format

---
+++ app/src/main/java/com/ternaryop/utils/date/IsoFormat.kt
@@ -0,0 +1,15 @@
+package com.ternaryop.utils.date
+
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+private val ISO_DATE_FORMAT = SimpleDateFormat("yyyy-MM-dd", Locale.US)
+
+fun Calendar.fromIsoFormat(isoDateString: String): Calendar {
+    val c = Calendar.getInstance()
+    c.time = ISO_DATE_FORMAT.parse(isoDateString)
+    return c
+}
+
+fun Calendar.toIsoFormat(): String = ISO_DATE_FORMAT.format(time)
