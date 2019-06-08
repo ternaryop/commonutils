e70fb68a15236fbbae5619fcb79c639ef1dfbb10
Davide Ficano <davide.ficano@gmail.com>
2019-06-08 09:52:59 +0200

    deprecated shareImage versus showShareChooser

--- app/src/main/java/com/ternaryop/utils/intent/ShareUtils.kt
+++ app/src/main/java/com/ternaryop/utils/intent/ShareUtils.kt
@@ -3,9 +3,28 @@ package com.ternaryop.utils.intent
 import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.provider.MediaStore
 
 object ShareUtils {
+    fun showShareChooser(context: Context, uri: Uri, mimeType: String, subject: String, title: String) {
+        val intent = Intent(Intent.ACTION_SEND)
+            .setDataAndType(uri, mimeType)
+            .putExtra(Intent.EXTRA_SUBJECT, subject)
+            .putExtra(Intent.EXTRA_STREAM, uri)
+            .addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT)
+            .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+
+        context.startActivity(Intent.createChooser(intent, title))
+    }
+
+    @Deprecated(
+        message = "Do not property work with Api level 29, use showShareChooser",
+        replaceWith = ReplaceWith(
+            expression = "showShareChooser",
+            imports = ["com.ternaryop.utils.intent.ShareUtils.showShareChooser"]
+        ),
+        level = DeprecationLevel.WARNING)
     fun shareImage(context: Context, fullPath: String, mimeType: String, subject: String, chooserTitle: String) {
         val values = ContentValues(4)
         values.put(MediaStore.Images.Media.MIME_TYPE, mimeType)
